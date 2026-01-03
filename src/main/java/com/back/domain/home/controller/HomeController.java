d0fd59894a79ae980cd23b28eedcb7edda6784f1
SangWon7242 <gotkdqja@gmail.com>
2026-01-03 09:33:21 +0900

    work

--- src/main/java/com/back/domain/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
   @GetMapping
   public String home() {
-    return "Hello!";
+    return "메인 홈입니다!!";
   }
 }
